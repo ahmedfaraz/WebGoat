174c49016a738c8fe614288aa5c827207c55e879
AhmedFaraz <ahmedfaraz1989@gmail.com>
2025-12-18 15:10:07 +0000

    4th_fix

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/Servers.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/Servers.java
@@ -6,6 +6,8 @@ package org.owasp.webgoat.lessons.sqlinjection.mitigation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -22,50 +24,65 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class Servers {
 
-  private final LessonDataSource dataSource;
+    private final LessonDataSource dataSource;
 
-  @AllArgsConstructor
-  @Getter
-  private class Server {
+    @AllArgsConstructor
+    @Getter
+    private class Server {
 
-    private String id;
-    private String hostname;
-    private String ip;
-    private String mac;
-    private String status;
-    private String description;
-  }
-
-  public Servers(LessonDataSource dataSource) {
-    this.dataSource = dataSource;
-  }
-
-  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  @ResponseBody
-  public List<Server> sort(@RequestParam String column) throws Exception {
-    List<Server> servers = new ArrayList<>();
-
-    try (var connection = dataSource.getConnection()) {
-      try (var statement =
-          connection.prepareStatement(
-              "select id, hostname, ip, mac, status, description from SERVERS where status <> 'out"
-                  + " of order' order by "
-                  + column)) {
-        try (var rs = statement.executeQuery()) {
-          while (rs.next()) {
-            Server server =
-                new Server(
-                    rs.getString(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4),
-                    rs.getString(5),
-                    rs.getString(6));
-            servers.add(server);
-          }
-        }
-      }
+        private String id;
+        private String hostname;
+        private String ip;
+        private String mac;
+        private String status;
+        private String description;
     }
-    return servers;
-  }
+
+    public Servers(LessonDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<Server> sort(@RequestParam String column) throws Exception {
+        List<Server> servers = new ArrayList<>();
+
+        // Whitelist of allowed sort columns
+        Map<String, String> ALLOWED_COLUMNS = Map.of(
+            "id", "id",
+            "hostname", "hostname",
+            "ip", "ip",
+            "mac", "mac",
+            "status", "status",
+            "description", "description"
+        );
+
+        // Use a safe default if the requested column is not allowed
+        String orderBy = ALLOWED_COLUMNS.getOrDefault(column, "id");
+
+        String sql =
+            "SELECT id, hostname, ip, mac, status, description " +
+            "FROM SERVERS " +
+            "WHERE status <> 'out of order' " +
+            "ORDER BY " + orderBy;
+
+        try (var connection = dataSource.getConnection();
+             var statement = connection.prepareStatement(sql);
+             var rs = statement.executeQuery()) {
+
+            while (rs.next()) {
+                Server server =
+                    new Server(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6));
+                servers.add(server);
+            }
+        }
+
+        return servers;
+    }
 }
